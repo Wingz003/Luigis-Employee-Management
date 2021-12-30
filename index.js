6ebbbf1f8654f699b935517f6dacd931be691db0
Wingz003 <Lsanti618@gmail.com>
2021-12-29 22:58:39 -0600

    added choices to list, added options to switch statement, added tables to db

--- index.js
+++ index.js
@@ -8,13 +8,39 @@ async function startProgram() {
         name: "choice",
         type: "list",
         message: "what do you want to do?",
-        choices: ["update role", "show employees"],
+        choices: [
+            "view all departments",
+            "view all roles",
+            "view all employees",
+            "add a department",
+            "add a role",
+            "add an employee",
+            "update role"
+        ],
 
 
     }])
     console.log(choice);
 
     switch (choice) {
+        case "view all departments":
+            viewDepartments()
+            break;
+        case "view all roles":
+            viewRoles()
+            break;
+        case "view all employees":
+            viewEmployees()
+            break;
+        case "add a department":
+            addDepartment()
+            break;
+        case "add a role":
+            addRole()
+            break;
+        case "add an employee":
+            addEmployee()
+            break;
         case "update role":
             updateRole()
             break;
@@ -28,7 +54,7 @@ async function startProgram() {
 async function updateRole() {
   // create the connection
   const connection = await mysql.createConnection({host:'localhost', user: 'root', database: 'employee_db'});
-  // query database
+  
   const [rows, fields] = await connection.execute("select * from employee");
 
  const newChoices = rows.map(employee => ({name:employee.name, value:employee}))  
@@ -48,4 +74,50 @@ async function updateRole() {
     console.log(choice);
 
     //Make a new query. Based on what ever choice i made, i can update the database or delete from the database
+}
+
+async function viewDepartments() {
+    const connection = await mysql.createConnection({host:'localhost', user: 'root', database: 'employee_db'});
+  
+  const [rows, fields] = await connection.execute("select * from department");
+
+ 
+
+  console.table(rows);
+}
+
+async function viewRoles() {
+    const connection = await mysql.createConnection({host:'localhost', user: 'root', database: 'employee_db'});
+    
+    const [rows, fields] = await connection.execute("select * from roles");
+  
+   
+  
+    console.table(rows);
+}
+
+async function viewEmployees() {
+    const connection = await mysql.createConnection({host:'localhost', user: 'root', database: 'employee_db'});
+    
+    const [rows, fields] = await connection.execute("select * from employee");
+
+    console.table(rows);
+  
+}
+
+async function addDepartment() {
+    const connection = await mysql.createConnection({host:'localhost', user: 'root', database: 'employee_db'});
+    const {newDepartment} = await inquirer.prompt([{
+        name: "newDepartment",
+        type: "input",
+        message: "What department would like to add?",
+        
+        
+    }]) 
+    
+    const [rows, fields] = await connection.execute(`INSERT INTO department (id, name) VALUES ("${newDepartment}")`);
+    
+    
+    console.table(rows);
+    
 }
--- schema.sql
+++ schema.sql
@@ -1,13 +1,41 @@
-
+DROP DATABASE employee_db;
 CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE employee(
-id int primary key auto_increment,
-name varchar(30),
-role_id int
-
+CREATE TABLE department(
+    id INT primary key auto_increment,
+    name VARCHAR(30)
 );
 
-INSERT INTO employee (id, name, role_id) values (1, "Jason Day", 1);
+CREATE TABLE roles(
+    id int primary key auto_increment,
+    title varchar(30),
+    salary DECIMAL NOT NULL,
+    department_id int NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee(
+    id int primary key auto_increment,
+    first_name varchar(30),
+    last_name varchar(30),
+    role_id int NOT NULL,
+    manager_id int,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
+
+
+
+
+
+
+
+INSERT INTO department (id, name) VALUES (1, "a");
+INSERT INTO department (id, name) VALUES (2, "b");
+INSERT INTO department (id, name) VALUES (3, "c");
+INSERT INTO roles (id, title, salary, department_id) VALUES (1, "engineer", 75000, 3);
+INSERT INTO roles (id, title, salary, department_id) VALUES (2, "intern", 35000, 3);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) values (1, "Jason", "Day", 1, NULL);
